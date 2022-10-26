eed413e7e0aef6759bc54d828f102e6206da8763
Anthony Iritano <airitano713@gmail.com>
2022-10-26 11:19:25 -0400

    carousels,details component and polishing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useBreedList from "./useBreedList";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -6,67 +8,89 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    requestPets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+    setPets(json.pets);
+  }
   return (
     <div className="search-params">
-      <form>
-        <label htmlFor="location">
-          location
-          <input
-            id="location"
-            value={location}
-            placeholder="location"
-            onChange={(e) => setLocation(e.target.value)}
-          ></input>
-        </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(e) => {
-              setAnimal(e.target.value);
-              setBreed("");
-            }}
-            onBlur={(e) => {
-              setAnimal(e.target.value);
-              setBreed("");
-            }}
-          >
-            <option />
-            {ANIMALS.map((animal) => {
-              return (
-                <option key={animal} value={animal}>
-                  {animal}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(e) => {
-              setBreed(e.target.value);
-            }}
-            onBlur={(e) => {
-              setBreed(e.target.value);
-            }}
-          >
-            <option />
-            {breeds.map((breed) => {
-              return (
-                <option key={breed} value={breed}>
-                  {breed}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <button>Submit</button>
-      </form>
+      <div className="form-wrapper">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            requestPets();
+          }}
+        >
+          <label htmlFor="location">
+            location
+            <input
+              id="location"
+              value={location}
+              placeholder="location"
+              onChange={(e) => setLocation(e.target.value)}
+            ></input>
+          </label>
+          <label htmlFor="animal">
+            Animal
+            <select
+              id="animal"
+              value={animal}
+              onChange={(e) => {
+                setAnimal(e.target.value);
+                setBreed("");
+              }}
+              onBlur={(e) => {
+                setAnimal(e.target.value);
+                setBreed("");
+              }}
+            >
+              <option />
+              {ANIMALS.map((animal) => {
+                return (
+                  <option key={animal} value={animal}>
+                    {animal}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+          <label htmlFor="breed">
+            Breed
+            <select
+              id="breed"
+              value={breed}
+              onChange={(e) => {
+                setBreed(e.target.value);
+              }}
+              onBlur={(e) => {
+                setBreed(e.target.value);
+              }}
+            >
+              <option />
+              {breeds.map((breed) => {
+                return (
+                  <option key={breed} value={breed}>
+                    {breed}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+          <button>Submit</button>
+        </form>
+      </div>
+      <Results pets={pets} />
     </div>
   );
 };
